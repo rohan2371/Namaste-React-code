8f2f4bd95946de608044663c5eacd8b765ceb857
rohan2371 <rohankonkatti2371@gmail.com>
2023-07-24 08:53:14 +0530

    created list using create.reactelemenet

--- App.js
+++ App.js
@@ -10,14 +10,36 @@ const heading1 = React.createElement(
         className: "header_tag",
         style: {
             color: "red",
+            
         },
+        key :"h1-tag"
     },
     "heading 1"
 );
 
-const heading2 = React.createElement("h2", {}, "heading 2");
+// console.log(heading1)
+const heading2 = React.createElement("h2", {key : "h2-tag"}, "heading 2");
+
+let heading3 = React.createElement("h3",{key:"h3-tag"},"TCS Zindabad");
+
+
+let li1 = React.createElement("li",{},"about us");
+let li2 = React.createElement("li",{},"home");
+
+let ul = React.createElement("ul",{},[li1,li2,React.createElement("li",{},"payment")]);
 const container = React.createElement("div", {
     className:"container"
-}, [heading1, heading2]);
+}, [heading1, heading2,heading3,ul]);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
+
+
+
+
+
+
+
+// babel-plugin-transform-remove-console     
